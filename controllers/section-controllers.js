f1c1f9ebe924250bef74e5b62f0f8be57564611c
Edifie <dilantaskin2@hotmail.com>
2023-01-12 17:53:27 +0000

    added update team members

--- controllers/section-controllers.js
+++ controllers/section-controllers.js
@@ -31,44 +31,40 @@ const createSection = (req, res, next) => {
 
   if (team) {
     for (let i = 0; i < team.length; i++) {
-      let file = files[i];
-      let item = team[i];
-      let img = fs.readFileSync(file.path);
-      let decode_image = img.toString("base64");
+      if (files[i]) {
+        let file = files[i];
+        let item = team[i];
+        let img = fs.readFileSync(file.path);
+        let decode_image = img.toString("base64");
 
-      // create an array to store the decoded images
-      let decodedImages = [];
+        // create an array to store the decoded images
+        let decodedImages = [];
 
-      decodedImages.push({
-        filename: file.originalname,
-        contentType: file.mimetype,
-        imageBase64: decode_image,
-      });
-
-      newSection.team.push({
-        memberName: item.memberName,
-        memberJobTitle: item.memberJobTitle,
-        memberDescription: item.memberDescription,
-        images: decodedImages,
-      });
+        decodedImages.push({
+          filename: file.originalname,
+          contentType: file.mimetype,
+          imageBase64: decode_image,
+        });
+        newSection.team.push({
+          memberName: item.memberName,
+          memberJobTitle: item.memberJobTitle,
+          memberDescription: item.memberDescription,
+          images: decodedImages,
+        });
+      }
     }
   }
 
   // save the object to the collection
-  Section.findOneAndUpdate(
-    { pageId: pageId },
-    newSection,
-    { upsert: true, new: true, useFindAndModify: false },
-    (error, doc) => {
-      if (error) {
-        res.status(500).json({ error: error.message });
-      } else {
-        res
-          .status(200)
-          .json({ message: "Section created/updated successfully" });
-      }
-    }
-  );
+  let newUpload = new Section(newSection);
+  newUpload
+    .save()
+    .then(() => {
+      res.status(200).json({ message: "Sections saved successfully" });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
 };
 
 // GET http://localhost:8080/api/pages/:pid/aditional-section
@@ -93,5 +89,92 @@ const getSectionByPageId = (req, res, next) => {
     });
 };
 
+const updateSection = async (req, res, next) => {
+  const files = req.files;
+  const {
+    pageId,
+    welcomeTitle,
+    welcomeDescription,
+    aboutUsTitle,
+    aboutUsDescription,
+    team,
+    teamTitle,
+  } = req.body;
+  let updates = {};
+
+  //update other fields if they are provided in the request body
+  if (welcomeTitle) {
+    updates.welcomeTitle = welcomeTitle;
+  }
+  if (welcomeDescription) {
+    updates.welcomeDescription = welcomeDescription;
+  }
+  if (aboutUsTitle) {
+    updates.aboutUsTitle = aboutUsTitle;
+  }
+  if (aboutUsDescription) {
+    updates.aboutUsDescription = aboutUsDescription;
+  }
+  if (teamTitle) {
+    updates.teamTitle = teamTitle;
+  }
+
+  if (team) {
+    let teamPromises = []; //create an empty array to store promises for updating or creating team members
+    team.forEach((item, i) => { // item -> current team member being processed, i-> index in the array
+      let decodedImages = [];
+      if (files[i]) {
+        let file = files[i];
+        let img = fs.readFileSync(file.path);
+        let decode_image = img.toString("base64");
+
+        decodedImages.push({
+          filename: file.originalname,
+          contentType: file.mimetype,
+          imageBase64: decode_image,
+        });
+      }
+      let teamMember = {
+        memberName: item.memberName,
+        memberJobTitle: item.memberJobTitle,
+        memberDescription: item.memberDescription,
+        images: decodedImages,
+      };
+      if (item._id) {
+        //update existing team member
+        teamPromises.push(
+          Section.findByIdAndUpdate(
+            item._id,
+            { $set: teamMember },
+            { new: true }
+          )
+        );
+      } else {
+        //create new team member
+        let newTeamMember = new Section(teamMember);
+        teamPromises.push(newTeamMember.save());
+      }
+    });
+
+    Promise.all(teamPromises)
+      .then((result) => {
+        res.status(200).json({ message: "Section updated successfully!" });
+      })
+      .catch((error) => {
+        res.status(500).json({ error: error.message });
+      });
+  } else {
+    //update other fields if no team member provided
+    Section.findOneAndUpdate({ pageId: pageId }, updates)
+      .then(() => {
+        res.status(200).json({ message: "Section updated successfully!" });
+      })
+      .catch((error) => {
+        res.status(500).json({ error: error.message });
+      });
+  }
+};
+
 exports.createSection = createSection;
 exports.getSectionByPageId = getSectionByPageId;
+exports.updateSection = updateSection;
